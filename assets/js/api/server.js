ee0ce709ec5d981592793c95e2f9c8740e14c749
Konstantin Danovich <KDanovich42@gmail.com>
2023-03-25 03:28:32 +0300

    restructured folders, added static paths, server returns index.html

--- assets/js/api/server.js
+++ assets/js/api/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cors from 'cors';
 import apiGetID from './getID.js';
 import apiCrawl from './crawl.js';
@@ -18,6 +20,15 @@ app.use(cors());
 app.use('/api/getID', apiGetID);
 app.use('/api/crawl', apiCrawl);
 app.use('/api/clear', apiClear);
+
+// Serve static assets
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route handler for the the URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Start the server
 const port = process.env.PORT || 3000;
