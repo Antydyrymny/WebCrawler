d6425722ea4ee5513f05a02840a95879c2809824
Konstantin Danovich <KDanovich42@gmail.com>
2023-03-26 18:18:30 +0300

    testing on localhost

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,9 +1,8 @@
 import { createGraph } from './graph.js';
 import { initialData } from './initialData.js';
 
-// const route = `https://graph-crawler.vercel.app/api/`;
-// const route = `http://localhost:3000/api`;
-const route = 'https://graph-crawler-server.vercel.app/api//api';
+const route = `http://localhost:3000/api`;
+// const route = 'https://graph-crawler-server.vercel.app/api//api';
 
 const form = document.querySelector('form');
 const graph = document.querySelector('.graph');
