b65dba60b43dd80402e80d325e5815d956bc9433
Konstantin Danovich <KDanovich42@gmail.com>
2023-03-27 18:00:07 +0300

    removed console.logs

--- server/api/deleteID.js
+++ server/api/deleteID.js
@@ -6,7 +6,5 @@ const router = express.Router();
 // Handle delete user session ID after he leaves the webpage
 export default router.post('/', (req, _) => {
     const uniqueId = req.body;
-    console.log(uniqueId);
     userData.delete(uniqueId);
-    console.log(userData);
 });
