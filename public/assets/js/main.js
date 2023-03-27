65c1952c17d9c2835fdf96623803917edc9cba55
Konstantin Danovich <KDanovich42@gmail.com>
2023-03-27 16:41:38 +0300

    installed dotenv to public

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,8 @@
 import { createGraph } from './graph.js';
 import { initialData } from './initialData.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const route = process.env.route;
 // const route = `http://localhost:3000/api`;
 // const route = 'https://graph-crawler-server.vercel.app/api//api';
