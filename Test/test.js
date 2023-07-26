05e9d885d8ae4f485828787f58b99bc543b6e8c8
Konstantin Danovich <KDanovich42@gmail.com>
2023-07-26 21:32:36 +0300

    reformat

--- Test/test.js
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from '@jest/globals';
-import { normalizeURL } from '../JS/crawler.js';
-
-test('normalizeURL', () => {
-    const input = '';
-    const output = normalizeURL();
-    const expected = 1;
-    expect(output).toEqual(expected);
-});
